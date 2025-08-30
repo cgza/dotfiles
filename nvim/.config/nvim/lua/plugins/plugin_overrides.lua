a4e7828afa0dfbcdcb2ef7d6c8b37088803aba80
carloz <carlos@MacBookAir.local>
2025-08-30 17:51:42 +0200

    fix for bufferline plugin problem with catpuccin theme

--- nvim/.config/nvim/lua/plugins/plugin_overrides.lua
+++ nvim/.config/nvim/lua/plugins/plugin_overrides.lua
@@ -35,4 +35,16 @@ return {
       })
     end,
   },
+
+  -- temporary fix to solve bufferline plugin integration with catpuccin
+  {
+    "catppuccin/nvim",
+    opts = function(_, opts)
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
+  },
 }
